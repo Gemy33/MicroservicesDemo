ef2602f82f80b87025a701cff6b960076ceb6496
Mohamed Gamal Abdel Halim <mohamedgamalabdelhaliym@gmail.com>
2026-08-11 19:02:52 +0300

    feat: add product and order microservices and communicate through http

---
+++ OrderService/Controllers/OrdersController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderService.Models;
+
+namespace OrderService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrdersController : ControllerBase
+{
+    private readonly HttpClient _httpClient;
+
+    private static readonly List<Order> Orders =
+    [
+        new Order
+        {
+            Id = 1,
+            ProductId = 1,
+            Quantity = 2
+        },
+        new Order
+        {
+            Id = 2,
+            ProductId = 2,
+            Quantity = 5
+        }
+    ];
+
+    public OrdersController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient();
+    }
+
+    [HttpGet]
+    public IActionResult GetOrders()
+    {
+        return Ok(Orders);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetOrder(int id)
+    {
+        var order = Orders.FirstOrDefault(x => x.Id == id);
+
+        if (order is null)
+            return NotFound();
+
+        var response = await _httpClient.GetAsync(
+            $"https://localhost:7274/api/products/{order.ProductId}");
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return StatusCode(503, "Product service is unavailable.");
+        }
+
+        var product = await response.Content.ReadFromJsonAsync<ProductDto>();
+
+        return Ok(new
+        {
+            order.Id,
+            order.ProductId,
+            ProductName = product?.Name,
+            ProductPrice = product?.Price,
+            order.Quantity
+        });
+    }
+}
---
+++ ProductService/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductService.Models;
+
+namespace ProductService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private static readonly List<Product> Products =
+    [
+        new Product { Id = 1, Name = "Laptop", Price = 1000 },
+        new Product { Id = 2, Name = "Mouse", Price = 30 },
+        new Product { Id = 3, Name = "Keyboard", Price = 80 }
+    ];
+
+    [HttpGet]
+    public IActionResult GetProducts()
+    {
+        return Ok(Products);
+    }
+
+    [HttpGet("{id:int}")]
+    public IActionResult GetProduct(int id)
+    {
+        var product = Products.FirstOrDefault(x => x.Id == id);
+
+        if (product is null)
+            return NotFound();
+
+        return Ok(product);
+    }
+}
