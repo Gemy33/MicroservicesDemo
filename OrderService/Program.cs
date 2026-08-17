4235413e3f6ae58f460ee34c45294ba23dc186c2
Mohamed Gamal Abdel Halim <mohamedgamalabdelhaliym@gmail.com>
2026-08-17 19:29:36 +0300

    feat: add resilience and observability

--- OrderService/Program.cs
+++ OrderService/Program.cs
@@ -1,12 +1,35 @@
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracing =>
+    {
+        tracing
+            .AddSource("OrderService")
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddConsoleExporter();
+    });
 builder.Services.AddHttpClient("ProductService", client =>
 {
     client.BaseAddress = new Uri(
         builder.Configuration["ProductService:BaseUrl"]!
     );
+})
+.AddStandardResilienceHandler(options =>
+{
+    options.Retry.MaxRetryAttempts = 3;
+    options.Retry.Delay = TimeSpan.FromSeconds(1);
+
+    options.TotalRequestTimeout.Timeout = TimeSpan.FromSeconds(10);
+
+    options.CircuitBreaker.SamplingDuration = TimeSpan.FromSeconds(30);
+    options.CircuitBreaker.FailureRatio = 0.5;
+    options.CircuitBreaker.MinimumThroughput = 5;
+    options.CircuitBreaker.BreakDuration = TimeSpan.FromSeconds(10);
 });
 
 var app = builder.Build();
